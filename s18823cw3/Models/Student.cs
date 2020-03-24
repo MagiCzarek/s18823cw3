f0aa50ca88af97604e9ffb045cf80ffd425ad203
Cezary Boguszewski <s18823@pjwstk.edu.pl>
2020-03-24 22:09:09 +0100

    Dodaj pliki projektów.

---
+++ s18823cw3/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace s18823cw3.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LasName { get; set; }
+
+        public string IndexNumber { get; set; }
+    }
+}
